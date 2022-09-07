4f4de726484bbc9a036cf60495629076d48586e0
Tuan Tran <hometran@gmail.com>
2022-09-06 22:16:45 -0700

    Finish Test

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,26 @@
-from django.db import models # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None):
+        pass
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
